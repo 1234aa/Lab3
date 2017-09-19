cd3551caa3b8d26b6d54c7a32f126e28dcca4281
abcdefg <abcdefg@host-229-15.ilcupre.champaign.il.us.clients.pavlovmedia.net>
2017-09-19 16:47:38 -0500

    hhhh

---
+++ src/main/java/Webpage.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+public class Webpage {
+
+
+    /**
+     * Retrieve contents from a URL and return them as a string.
+     *
+     * @param url url to retrieve contents from
+     * @return the contents from the url as a string, or an empty string on error
+     */
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
+    }
+    public static void main(String[] args) {
+
+        System.out.println(urlToString("http://erdani.com/tdpl/hamlet.txt"));
+        int index = -1;
+        int count = 0;
+        String source ="http://erdani.com/tdpl/hamlet.txt";
+        while(source.indexOf(".", ++index) != -1)
+            count++;
+        System.out.println(count);
+    }
+}
